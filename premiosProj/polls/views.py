045c3cad61cac29db78d6e472d9e7e8748a47c2c
NiengLee <ncyl91@gmail.com>
2023-04-26 01:00:35 -0500

    creacion de views y especificacion

--- premiosProj/polls/views.py
+++ premiosProj/polls/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello world!')
+    return HttpResponse('pagina princiapl de premios')
+
+def detail(request, question_id):
+    return HttpResponse(f"estas viendo la pregunta numero {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"estas viendo los resultados de la pregunta numero {question_id}")
