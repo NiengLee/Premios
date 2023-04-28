d10e10b8b8536712ebe04eb7202589fc33dc945c
NiengLee <ncyl91@gmail.com>
2023-04-28 02:00:41 -0500

    se creo el archivo details

--- premiosProj/polls/views.py
+++ premiosProj/polls/views.py
@@ -7,10 +7,16 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.all()
-    return HttpResponse('pagina princiapl de premios')
+    return render(request, "polls/index.html", {
+        "latest_question_list" : latest_question_list,
+        })
 
 def detail(request, question_id):
-    return HttpResponse(f"estas viendo la pregunta numero {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "quetion": question
+    })
+    
 
 def results(request, question_id):
     return HttpResponse(f"estas viendo los resultados de la pregunta numero {question_id}")
