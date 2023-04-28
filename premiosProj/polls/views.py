220c165b4a16feffc1392821c616e19bd670c97d
NiengLee <ncyl91@gmail.com>
2023-04-28 01:45:16 -0500

    se crea ya los templates y empezamos el frontend

--- premiosProj/polls/views.py
+++ premiosProj/polls/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
+    latest_question_list = Question.objects.all()
     return HttpResponse('pagina princiapl de premios')
 
 def detail(request, question_id):
